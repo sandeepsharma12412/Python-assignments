50d5db887a0bf961e092525e47951c9e474ab24c
sandeep sharma <sandeep12412@gmail.com>
2025-07-31 17:01:30 +0530

    Create Assignment1-task1.py
    Assignment1 task1  file

---
+++ Assignment1-task1.py
@@ -0,0 +1,10 @@
+a = input('Enter the first number:')
+b = input('Enter the second number:')
+a=int(a)
+b=int(b)
+
+print('Addition: ', a+b)
+print('Subtraction: ', a-b)
+print('Multiplication: ',a*b)
+print('Division: ',a/b)
+
