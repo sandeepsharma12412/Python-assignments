32d5f7eec7ab9427a8b92762f6f850979f995a2a
sandeep sharma <sandeep12412@gmail.com>
2025-07-31 17:14:33 +0530

    Create Assignment1-task2.py
    Assignment1 task 2 files

---
+++ Assignment1-task2.py
@@ -0,0 +1,9 @@
+#input from user
+firstname=input('Enter your first name: ')
+#input from user
+lastname=input('Enter your last name: ')
+#input value first latter capitalize
+firstname=firstname.capitalize()
+lastname=lastname.capitalize()
+
+print('hello,',firstname,lastname+'!welcome to the Python program.')
