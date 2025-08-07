151f5672c486e751b35e632fdeadf9344cdf1048
sandeep sharma <sandeep12412@gmail.com>
2025-08-07 10:29:19 +0530

    Create Assignement5-task2.py
    Assignement5 task2.py

---
+++ Assignement5-task2.py
@@ -0,0 +1,6 @@
+originallist =[1,2,3,4,5,6,7,8,9,10]
+print('Original List:',originallist)
+sliceList =  originallist[0:5]
+print('Extracted First five Elements:',sliceList)
+reverselist = list(reversed(sliceList))
+print('Revesed Extracred elements',reverselist)
